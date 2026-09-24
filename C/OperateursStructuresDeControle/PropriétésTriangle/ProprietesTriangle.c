3736d221eef3eccf871d941a3342156634f6098a
Emi <118975094+fridasola@users.noreply.github.com>
2026-09-24 22:06:58 +0200

    Propriété du triangle
    Tp2 Ex8 : Ecrire un programme qui lit trois réels représentant les longueurs des côtés d'un triangle et qui affiche la ou les propriété(s) du triangle.

--- C/OperateursStructuresDeControle/PropriétésTriangle/ProprietesTriangle.c
+++ C/OperateursStructuresDeControle/PropriétésTriangle/ProprietesTriangle.c
@@ -5,13 +5,17 @@ int main()
     int a,b,c;
     printf("Entrez les longueurs des trois côtés du triangle: ");
     scanf("%d %d %d",&a,&b,&c);
+    
     if (a==b && b==c) {
         printf("Le triangle est équilatéral.\n");
-    } else if (a==b || b==c || a==c) {
+    }
+    if (a==b || b==c || a==c) {
         printf("Le triangle est isocèle.\n");
-    } else if (a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b) {
+    }
+    if (a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b) {
         printf("Le triangle est rectangle.\n");
-    } else {
+    } 
+    if (a!=b && b!=c && a!=c && a*a!=b*b+c*c && b*b!=a*a+c*c && c*c!=a*a+b*b) {
         printf("Le triangle est quelconque.\n");
     }
     return 0;
